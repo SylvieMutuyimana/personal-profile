6383fb3ded3beef21edc218e9964a78fd32a90b6
Sylvie Mutuyimana <s.mutuyiman@alustudent.com>
2024-05-16 01:27:24 +0200

    css stylinga

--- components/bac/BackgroundWave.js
+++ components/bac/BackgroundWave.js
@@ -15,7 +15,6 @@ export const BackgroundWave = () => {
         </svg>
       </div>
       <div>
-        <svg className={styles.waves} xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
         viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
           <defs>
             <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
