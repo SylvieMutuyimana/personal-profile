f0a4b80bf9f051314a5d7fa63bf49c9a71b941e6
Sylvie Mutuyimana <s.mutuyiman@alustudent.com>
2024-05-16 01:25:08 +0200

    some animations

--- components/bac/BackgroundWave.js
+++ components/bac/BackgroundWave.js
@@ -21,7 +21,6 @@ export const BackgroundWave = () => {
             <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
           </defs>
           <g className={styles.parallax}>
-            <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
             <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(81, 216, 234, 0.4)" />
             <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(81, 216, 234, 0.8)" />
             <use xlink:href="#gentle-wave" x="48" y="7" fill="#51EAEA" />
