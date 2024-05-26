6308e0b570c881d62b820c464b14f5da1b2ac563
Sylvie Mutuyimana <s.mutuyiman@alustudent.com>
2024-05-26 15:17:17 +0200

    Fixing font

---
+++ components/education_experience.js
@@ -0,0 +1,34 @@
+// In Projects.js
+import React from 'react';
+import componentStyles from '../styles/module.css/components.module.css';
+import styles from '../styles/module.css/about.module.css';
+
+const Education_Exp = () => {
+  const theData = {
+    'education':[
+      {
+        school:'SOS Herman Gmeiner International College', degree:'IGCSE Certificate',
+        course:'IGCSE Certificate', course:'CS, Math, Physics, Chem', 
+      },
+      {
+        school:'SOS Herman Gmeiner International College', degree:'International Baccalaurette Diploma',
+        course:'ITGS, Math, Physics, Soc. & cult. Anthropology', 
+      },
+      {
+        school:'African Leadership University', degree:'Bachelor of Science', course: 'Computer Science',
+        start: '2021', end: '2024'
+      }
+    ], 
+    'experience':[
+      {}
+    ]
+  }
+    return (
+      <div className={`${componentStyles.grid} ${componentStyles.nogrid}`}>
+        <section id="Bio" className={styles.Education_Exp}>
+        </section>
+      </div>
+    );
+};
+
+export default Education_Exp;
