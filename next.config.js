468b9d0a68ebd5416d4ce87ec1c568506e24d25c
Sylvie Mutuyimana <s.mutuyiman@alustudent.com>
2024-05-29 22:38:24 +0200

    X-XSS-Protection

--- next.config.js
+++ next.config.js
@@ -1,24 +1,30 @@
-const { createSecureHeaders } = require('next-secure-headers');
-
 module.exports = {
   async headers() {
     return [
       {
         source: '/(.*)',
-        headers: createSecureHeaders({
-          contentSecurityPolicy: {
-            directives: {
-              defaultSrc: ["'self'"],
-              styleSrc: ["'self'", 'https://cdnjs.cloudflare.com'],
-              scriptSrc: ["'self'", "'unsafe-inline'"],
-              imgSrc: ["'self'", 'data:'],
-            },
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; style-src 'self' https://cdnjs.cloudflare.com; script-src 'self' 'unsafe-inline'; img-src 'self' data:",
           },
-          frameGuard: 'deny', // equivalent to X-Frame-Options: DENY
-          xssProtection: '1; mode=block', // equivalent to X-XSS-Protection: 1; mode=block
-          nosniff: 'nosniff', // equivalent to X-Content-Type-Options: nosniff
-          referrerPolicy: 'same-origin', // equivalent to Referrer-Policy: same-origin
-        }),
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin',
+          },
+        ],
       },
     ];
   },
